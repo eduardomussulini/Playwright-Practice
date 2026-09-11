df085be10e3b0f7e1cc8e8d369e3de499e7cd981
Eamussulini <eduardo.mussulini@gmail.com>
2026-09-10 21:33:36 -0300

    test tasks, locator by placeholder

---
+++ playwright-mark/tests/tasks.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test'
+
+test('deve poder cadastrar uma nova tarefa', async ({ page }) => {
+    await page.goto('http://localhost:8080')
+
+    await page.fill('input[class*=InputNewTask]', 'Ler um livro de TypeScript')
+
+})
